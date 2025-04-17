96120f45f8a9d45f4ad124897e481c5c3ee71d77
Eduardo da Silva Ferreira <eduardo.s.fereira@gmail.com>
2025-04-17 17:38:59 -0300

    commit

--- dio_blog/database.py
+++ dio_blog/database.py
@@ -11,11 +11,11 @@ database = databases.Database(settings.DATABASE_URL)
 if os.getenv("RENDER"):
     engine = sa.create_engine(
         settings.DATABASE_URL,
-        connect_args={"check_same_thread": False},
         echo=settings.DEBUG,
     )
 else:
     engine = sa.create_engine(
         settings.DATABASE_URL,
+        connect_args={"check_same_thread": False},
         echo=settings.DEBUG,
     )
